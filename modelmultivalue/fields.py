6fca09d567851b1bc94ea08319f0b0d406c5fe15
Todd Dembrey <todd.dembrey@gmail.com>
2016-07-07 14:30:22 +0100

    fields passed to ModelMultiValue now correctly handled

--- modelmultivalue/fields.py
+++ modelmultivalue/fields.py
@@ -6,39 +6,40 @@ from .widgets import ModelMultiValueWidget
 
 
 class ModelMultiValueField(forms.MultiValueField):
-    def __init__(self, model=None, *args, **kwargs):
+    def __init__(self, model=None, fields=ALL_FIELDS, *args, **kwargs):
         if model is None:
             raise ImproperlyConfigured('Field must be created with a model', 'no_model_provided')
         else:
             self.model = model
 
-        form_fields = kwargs.pop('fields', ALL_FIELDS)
-
-        if not form_fields == ALL_FIELDS:
+        if not fields == ALL_FIELDS:
             # Check if we've missed any required fields
             required_fields = set(name for name, field in forms.models.fields_for_model(self.model).items()
                                   if field.required)
-            missing_fields = required_fields - set(form_fields)
+            missing_fields = required_fields - set(fields)
             if missing_fields:
                 raise ImproperlyConfigured(
                     'The following required fields were missing: {}'.format(', '.join(missing_fields)),
                     'missing_required_fields')
 
         self.model_form = forms.models.modelform_factory(self.model,
-                                                         fields=form_fields)
-        kwargs.update({'required': False,
-                       'require_all_fields': False
-                       })
-        fields = [
+                                                         fields=fields)
+
+        form_fields = [
             field for field in self.model_form.base_fields.values()
         ]
 
-        self.widget = ModelMultiValueWidget(widgets=[field.widget for field in fields],
+        self.widget = ModelMultiValueWidget(widgets=[field.widget for field in form_fields],
                                             field_names=self.model_form.base_fields.keys(),
-                                            labels=[field.label for field in fields],
+                                            labels=[field.label for field in form_fields],
                                             model=self.model
                                             )
-        super(ModelMultiValueField, self).__init__(fields, *args, **kwargs)
+
+        kwargs.update({'required': False,
+                       'require_all_fields': False
+                       })
+
+        super(ModelMultiValueField, self).__init__(form_fields, *args, **kwargs)
 
     def compress(self, data_list):
         if data_list:
