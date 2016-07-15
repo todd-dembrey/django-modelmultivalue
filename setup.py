502f585009dc0d551e8be911d71c480c0c9020f7
Todd Dembrey <todd.dembrey@gmail.com>
2016-07-15 11:30:18 +0100

    Uprevved to v0.21

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-modelmultivalue',
-    version='0.20',
+    version='0.21',
     packages=find_packages(),
     install_requires=[
         'Django>=1.8',
